e148713c1e5e908ba073ed01b2faf2a881d22041
skepptical <tyrapael@gmail.com>
2026-05-04 21:14:01 -0500

    Delete week12-homework-theme-switcher directory

--- week12-homework-theme-switcher/scripts/app.js
+++
@@ -1,7 +0,0 @@
-function setTheme(themeName) {
-  const body = document.querySelector("body");
-  body.classList.remove("theme-two", "theme-three");
-  if (themeName !== "theme-one") {
-    body.classList.add(themeName);
-  }
-}
